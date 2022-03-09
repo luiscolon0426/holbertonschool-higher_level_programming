6798d571fecb0bcf356db20791ef4ec1924b96d8
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 15:49:43 +0000

    create user / create read user / force name / create new table

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creates database hbtn_0d_2 and the user_0d_2 / password?
+CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
+-- Create user if not exist
+CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost';
+-- sets privileges for the user created
+GRANT SELECT ON Hbtn_od_2.* TO 'user_0d_2' @'localhost';
