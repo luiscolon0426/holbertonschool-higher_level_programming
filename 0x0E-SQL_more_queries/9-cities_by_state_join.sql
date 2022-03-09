4e13b084128f6dce847b5108e21e2e16f9a5b5e8
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 17:33:52 +0000

    fixed space typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- script that lists all citiee in database
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.states_id=states.id
+FROM cities INNER JOIN states 
+ON cities.states_id = states.id
 ORDER BY cities.id;
