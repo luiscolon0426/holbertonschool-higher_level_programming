c12fec567308000a3aaa86aaf3b90ec2501c3afb
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 16:09:41 +0000

    deleted comments and fixed upper case

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
 -- Creates database hbtn_0d_2 and the user_0d_2 / password?
 CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
--- Create user if not exist
 CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
--- sets privileges for the user created
-GRANT SELECT ON Hbtn_od_2.* TO 'user_0d_2' @'localhost';
+GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2' @'localhost';
