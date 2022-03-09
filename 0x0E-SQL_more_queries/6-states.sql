7845f81b23b1411be9e467429e1820e2a1cd6f06
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 16:16:51 +0000

    del extra command

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- creates database, table in database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+CREATE TABLE IF NOT EXISTS states(
     id INT NOT PRIMARY KEY AUTO_INCREMENT,
-    name VARHCAR(256) NOT NULL);
+    name VARHCAR(256) NOT NULL
+);
