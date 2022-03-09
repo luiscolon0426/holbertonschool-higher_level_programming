b4387f9f98187480bf1af14c153f63cf522bf9bd
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 17:22:17 +0000

    list all cities in database

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- script that lists all citiee in database
 SELECT cities.id,
-    cities.namem states.name
+    cities.name, states.name
 FROM cities
     INNER JOIN states ON cities.states_id = states.id
 ORDER BY cities.id;
