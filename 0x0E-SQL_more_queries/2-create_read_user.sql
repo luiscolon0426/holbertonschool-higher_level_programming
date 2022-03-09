574b7d5610bb0dfdbeae2b057b1dc0d519ed33b3
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 16:10:51 +0000

    cmon man, should be fine

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- Creates database hbtn_0d_2 and the user_0d_2 / password?
-CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2' @'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2' @'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2' @'localhost';
