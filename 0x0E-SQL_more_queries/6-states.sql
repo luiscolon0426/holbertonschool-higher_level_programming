f9c9520b0ee2907cb555eaf545b582ac1a64aa15
luiscolon0426 <luiscolon0426@gmail.com>
2022-03-09 16:15:48 +0000

    typos typos typos

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates database, table in database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARHCAR(256) NOT NULL
-);
+    id INT NOT PRIMARY KEY AUTO_INCREMENT,
+    name VARHCAR(256) NOT NULL);
