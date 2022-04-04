96481b48ebcf84f2990d919e17cc2e575faae708
Luis Colon <luiscolon0426@gmail.com>
2022-04-04 12:24:40 -0400

    fixed multiple errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const i = process.argv.length;
-if (i > 3) {
-    console.log('Arguments found');
-} else if (i === 3) {
-    console.log('Argument found');
-} else {
-    console.log('No argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
+}
+if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
