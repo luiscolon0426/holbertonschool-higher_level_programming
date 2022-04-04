baef98b92d8c31bbf3ec7747300559eabfe5d62e
Luis Colon <luiscolon0426@gmail.com>
2022-04-04 13:24:47 -0400

    print loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const lst = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const length = lst.length;
+let i = 0;
+for (i = 0; i < length; i++) {
+  console.log(lst[i]);
+}
