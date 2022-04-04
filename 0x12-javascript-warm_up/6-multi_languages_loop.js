d52a958467de139410b429729658334bad3bac5e
Luis Colon <luiscolon0426@gmail.com>
2022-04-04 14:27:43 -0400

    TYPO in JS

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const lst = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 const length = lst.length;
 let i = 0;
 for (i = 0; i < length; i++) {
-  console.log(lst[i]);
+    console.log(lst[i]);
 }
