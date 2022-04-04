a0b6e37fffc64cfb7a235bf15479cb6fa79e6613
Luis Colon <luiscolon0426@gmail.com>
2022-04-04 14:29:25 -0400

    bad file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,5 +4,5 @@ const lst = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 const length = lst.length;
 let i = 0;
 for (i = 0; i < length; i++) {
-    console.log(lst[i]);
+  console.log(lst[i]);
 }
