4ca6b49105b87897c63327cbe7c9da469ed1e61c
Luis Colon <luiscolon0426@gmail.com>
2022-04-04 12:16:24 -0400

    fixed indentation again for checkers

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const a = process.argv.lenght;
 
 if (a === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (a === 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
