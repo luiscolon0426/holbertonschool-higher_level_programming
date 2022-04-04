a9d5d37a531092877885c19cb6a32fdc80c0b804
Luis Colon <luiscolon0426@gmail.com>
2022-04-04 12:06:32 -0400

    working with indentation

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const a = process.argv.length;
+
+if (a === 2) {
+  console.log('No argument');
+} else if (a === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
