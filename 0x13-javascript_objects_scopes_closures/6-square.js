88dca7bd49f31a6b24fac42d4d0e340a93a6febc
Luis Colon <luiscolon0426@gmail.com>
2022-04-05 12:42:03 -0400

    still with the checkers

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
- // Create an instance method that printst the rectangle using the char c
+// Create an instance method that printst the rectangle using the char c
 
-const NewSquare = require('./4-rectangle.js');
+const NewSquare = require('./5-square.js');
 
 module.exports = class Square extends NewSquare {
-    charPrint(c) {
-        if (c === undefined) {
-            this.print();
-        } else {
-            for (let i = 0; i < this.width; i++) {
-                console.log(c.repeat(this.height));
-            }
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
+      }
     }
+  }
 };
