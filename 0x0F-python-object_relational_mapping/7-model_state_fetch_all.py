f321587e57ed1b297ab3f792d33866388daaeafe
Luis Colon <luiscolon0426@gmail.com>
2022-04-07 22:42:33 -0400

    lists all objects from db

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' lists all obj from db '''
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+rows = session.query(State.id, State.name).from_statement(text(
+       "SELECT id, name "
+        "FROM states "
+        "ORDER BY states.id ASC "
+       )).all()
+for id, name in rows:
+    print("{:d}: {:s}".format(id, name))
+    session.close()
