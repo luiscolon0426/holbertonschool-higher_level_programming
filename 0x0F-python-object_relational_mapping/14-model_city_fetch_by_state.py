674824acc838f0b3a3066711dee52933cea2255a
Luis Colon <luiscolon0426@gmail.com>
2022-04-09 12:27:37 -0400

    checker friendly

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,28 +2,27 @@
 ''' prints city objects from db '''
 
 from sqlalchemy import create_engine, orm
-from sqlalchemy.orm import sessionmaker
-from sys import _xoptions, argv
-from model_state import State, Base
+import sqlalchemy
+
+from sys import argv
+
+from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    
-    user = argv[1]
-    passwd = argv[2]
-    host = 'localhost'
-    db = argv[3]
-    
-    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                           .format(user, passwd, host, db))
-    
-    session = orm.sessionmaker(bind=engine)()
-    Session = session()
-    
-    rows = Session.query(State.name, City.id, City.name).filter(
-        State.id == City.state_id).order_by(City.id)
-    
-    for row in rows:
-        print("{}: ({}) {}".format(row[0], row[1], row[2]))
-        
-    session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(*argv[1:4]), pool_pre_ping=True)
+
+Base.metadata.create_all(engine)
+
+session = orm.sessionmaker(bind=engine)()
+
+cities = (session
+          .query(State.name, City.id, City.name)
+          .filter(City.state_id == State.id)
+          .order_by(City.id))
+
+for city in cities:
+    print("{}: ({}) {}".format(*city))
+
+session.close()
