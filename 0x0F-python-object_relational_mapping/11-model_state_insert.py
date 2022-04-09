5d1f4a4da7f7796af589b58667b00d4097032e7e
Luis Colon <luiscolon0426@gmail.com>
2022-04-09 08:10:46 -0400

    fix requirements

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,11 +8,12 @@ from model_state import State, Base
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
+Base.metadata.create_all(eng)
+Session = sessionmaker(bind=eng)
 session = Session()
-newState = State(name="Louisiana")
-session.add(newState)
+new_state = State(name='Louisiana')
+session.add(new_state)
+state = session.query(State).filter_by(name='Louisiana').first()
+print(str(state.id))
 session.commit()
-print(newState.id)
 session.close()
