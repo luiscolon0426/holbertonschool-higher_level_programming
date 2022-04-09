5ac5c6093336bfaf6907f7e4b0afbad8f7ca71c4
Luis Colon <luiscolon0426@gmail.com>
2022-04-09 08:06:55 -0400

    pycodestyle

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,14 +9,11 @@ from model_state import State, Base
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-rows = session.query(State.id, State.name).from_statement(text(
-    "SELECT id, name "
-    "FROM states "
-    "WHERE name REGEXP '^.*a.*' "
-)).all()
-for id, name in rows:
-    print("{:d}: {:s}".format(id, name))
-session.close()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    s = '%a%'
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
