a248487be3715b9977bf2e82a1c11fcdfdc6e445
Luis Colon <luiscolon0426@gmail.com>
2022-04-21 12:36:24 -0400

    added file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-''' module with find_peak '''
+'''
+function that finds a peak in a list of unsorted integers
+'''
 
 
 def find_peak(list_of_integers):
-    """ comment """
+    '''
+    finds a peak in a list of unsorted integers
+    '''
     li = list_of_integers
-    leng = len(li)
-    if leng == 0:
+    if len(li) == 0:
         return None
-    if leng == 1:
+    if len(li) == 1:
         return
     if li[0] >= li[1]:
         return li[0]
     elif li[-1] >= li[-2]:
         return li[-1]
-    for i in range(1, leng - 1):
+    for i in range(1, len(li) - 1):
         if li[i] >= li[i - 1] and li[i] >= li[i + 1]:
             return li[i]
